4dd9ed24bec820108bea912ccc33edf905494164
Dhaesha M <dhaesha1@gmail.com>
2022-05-09 18:56:07 -0400

    header jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header_container">
+      <div className="header_top">
+        <h2>/DHAEDREAM/</h2>
+      </div>
+      <div className="header_banner"></div>
+    </div>
+  );
 };
 
 export default Header;
