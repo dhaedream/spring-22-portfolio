dd5d1ce263025fd5522c094c82e55b5607623cbe
Dhaesha M <dhaesha1@gmail.com>
2022-05-09 16:40:19 -0400

    imported logos as png images in header

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import ddLogo from "../../assets/dd-logo.png";
+import disLogo from "../../assets/discord-logo.png";
+import gitLogo from "../../assets/github-logo.png";
+import linLogo from "../../assets/linkedin-logo.png";
+
+// LogoLinkedin, LogoDiscord
 
 const Navbar = () => {
   return (
     <div className="navbar_container">
-      <img src={""} href="#home" />
+      <img src={ddLogo} href="#home" />
       <div className="navbar_links-container">
         <p>
           <a href="#about">ABOUT</a>
@@ -18,7 +24,17 @@ const Navbar = () => {
           <a href="#contact">CONTACT</a>
         </p>
       </div>
-      <div className="logo-container"></div>
+      <div className="navbar_logo-container">
+        <a>
+          <img src={linLogo} href="#linkedin" />
+        </a>
+        <a>
+          <img src={disLogo} href="#discord" />
+        </a>
+        <a>
+          <img src={gitLogo} href="#github" />
+        </a>
+      </div>
     </div>
   );
 };
