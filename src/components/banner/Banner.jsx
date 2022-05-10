9eed7426d3f42801a5e13849a7503415336b1a81
Dhaesha M <dhaesha1@gmail.com>
2022-05-10 11:07:49 -0400

    starting project component jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./banner.css";
+// import second from '../../assets/banner-dots.png'
 
 const Banner = () => {
   return <div className="banner"></div>;
---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div>
+      <div className="projects_outer-container">
+        <div className="projects_row-container">
+          <div className="projects_single-container">
+            <img />
+            <h3></h3>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
