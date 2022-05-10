88b4adbb0c392b6530cf1a32153de162162f835c
Dhaesha M <dhaesha1@gmail.com>
2022-05-09 20:14:22 -0400

    basic styling for desktop view of header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./header.css";
+import bannerImg from "../../assets/banner-dark.png";
 
 const Header = () => {
   return (
     <div className="header_container">
       <div className="header_top">
-        <h2>/DHAEDREAM/</h2>
+        <h2>DHAE DREAM</h2>
       </div>
-      <div className="header_banner"></div>
+      <img src={bannerImg} className="header_banner-img" />
     </div>
   );
 };
