684fac89529f1d6714f5a5d36ccb5ec927c7d719
Dhaesha M <dhaesha1@gmail.com>
2022-05-10 10:07:23 -0400

    basic features style in plaace

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -9,28 +9,32 @@ const Features = () => {
         <img src={skillsIcon} className="features_icon-img" />
         <h3 className="features_header">SKILLSET</h3>
         <p className="features_body-para">
-          Add a little bit of body text. Add a little bit of body text
+          Add a little bit of body text. Add a little bit of body text. Add a
+          little bit of body text. Add a little bit of body text
         </p>
       </div>
       <div className="features_single-column">
         <img src={skillsIcon} className="features_icon-img" />
         <h3 className="features_header">SKILLSET</h3>
         <p className="features_body-para">
-          Add a little bit of body text. Add a little bit of body text
+          Add a little bit of body text. Add a little bit of body text. Add a
+          little bit of body text. Add a little bit of body text
         </p>
       </div>
       <div className="features_single-column">
         <img src={skillsIcon} className="features_icon-img" />
         <h3 className="features_header">SKILLSET</h3>
         <p className="features_body-para">
-          Add a little bit of body text. Add a little bit of body text
+          Add a little bit of body text. Add a little bit of body text. Add a
+          little bit of body text. Add a little bit of body text
         </p>
       </div>
       <div className="features_single-column">
         <img src={skillsIcon} className="features_icon-img" />
         <h3 className="features_header">SKILLSET</h3>
         <p className="features_body-para">
-          Add a little bit of body text. Add a little bit of body text
+          Add a little bit of body text. Add a little bit of body text. Add a
+          little bit of body text. Add a little bit of body text
         </p>
       </div>
     </div>
