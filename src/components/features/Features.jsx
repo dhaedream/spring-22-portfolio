8ae042e1e90d3ebc5464114a03fffb0df38e1dc7
Dhaesha M <dhaesha1@gmail.com>
2022-05-10 09:54:22 -0400

    starting sttyle for features section

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,13 +1,38 @@
 import "./feature.css";
 import React from "react";
+import skillsIcon from "../../assets/light-dark.png";
 
 const Features = () => {
   return (
     <div className="features">
-      <div className="features_single-column">Skillset</div>
-      <div className="features_single-column">Background</div>
-      <div className="features_single-column">TECHNOLOGIES</div>
-      <div className="features_single-column">DHAE DREAMS</div>
+      <div className="features_single-column">
+        <img src={skillsIcon} className="features_icon-img" />
+        <h3 className="features_header">SKILLSET</h3>
+        <p className="features_body-para">
+          Add a little bit of body text. Add a little bit of body text
+        </p>
+      </div>
+      <div className="features_single-column">
+        <img src={skillsIcon} className="features_icon-img" />
+        <h3 className="features_header">SKILLSET</h3>
+        <p className="features_body-para">
+          Add a little bit of body text. Add a little bit of body text
+        </p>
+      </div>
+      <div className="features_single-column">
+        <img src={skillsIcon} className="features_icon-img" />
+        <h3 className="features_header">SKILLSET</h3>
+        <p className="features_body-para">
+          Add a little bit of body text. Add a little bit of body text
+        </p>
+      </div>
+      <div className="features_single-column">
+        <img src={skillsIcon} className="features_icon-img" />
+        <h3 className="features_header">SKILLSET</h3>
+        <p className="features_body-para">
+          Add a little bit of body text. Add a little bit of body text
+        </p>
+      </div>
     </div>
   );
 };
