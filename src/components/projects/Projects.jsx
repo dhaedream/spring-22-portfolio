5266200b2aa8bfccc434de35ea7680c7b8aad1ce
Dhaesha M <dhaesha1@gmail.com>
2022-05-10 17:51:54 -0400

    all over styling

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,7 +5,10 @@ import codeProject from "../../assets/solidity-code.jpeg";
 const Projects = () => {
   return (
     <div>
+      {/* <h1 className="projects_header">PROJECTS</h1> */}
       <div className="projects_outer-container">
+        <h1 className="projects_header">PROJECTS</h1>
+
         <div className="projects_row-container">
           <div className="projects_single-container">
             <img className="projects_img" src={codeProject} />
@@ -19,29 +22,35 @@ const Projects = () => {
           </div>
           <div className="projects_single-container">
             <img className="projects_img" src={codeProject} />
-            <h3>SMART CONTRACTS</h3>
-            <p>
-              Add a little bit of body text. Add a little bit of body text.Add a
-              little bit of body text.
-            </p>
+            <div className="projects_description-wrapper">
+              <h3>SMART CONTRACTS</h3>
+              <p>
+                Add a little bit of body text. Add a little bit of body text.Add
+                a little bit of body text.
+              </p>
+            </div>
           </div>
         </div>
         <div className="projects_row-container">
           <div className="projects_single-container">
             <img className="projects_img" src={codeProject} />
-            <h3>SMART CONTRACTS</h3>
-            <p>
-              Add a little bit of body text. Add a little bit of body text.Add a
-              little bit of body text.
-            </p>
+            <div className="projects_description-wrapper">
+              <h3>SMART CONTRACTS</h3>
+              <p>
+                Add a little bit of body text. Add a little bit of body text.Add
+                a little bit of body text.
+              </p>
+            </div>
           </div>
           <div className="projects_single-container">
             <img className="projects_img" src={codeProject} />
-            <h3>SMART CONTRACTS</h3>
-            <p>
-              Add a little bit of body text. Add a little bit of body text.Add a
-              little bit of body text.
-            </p>
+            <div className="projects_description-wrapper">
+              <h3>SMART CONTRACTS</h3>
+              <p>
+                Add a little bit of body text. Add a little bit of body text.Add
+                a little bit of body text.
+              </p>
+            </div>
           </div>
         </div>
       </div>
