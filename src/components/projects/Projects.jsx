cd9d9450d79ae8e184af8b0b0185672497ecd268
Dhaesha M <dhaesha1@gmail.com>
2022-05-11 18:27:38 -0400

    projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./projects.css";
 import codeProject from "../../assets/solidity-code.jpeg";
+import boredApe from "../../assets/bored-ape.png";
+import nft from "../../assets/nft.png";
 
 const Projects = () => {
   return (
@@ -11,22 +13,22 @@ const Projects = () => {
 
         <div className="projects_row-container">
           <div className="projects_single-container">
-            <img className="projects_img" src={codeProject} />
+            <img className="projects_img" src={boredApe} />
             <div className="projects_description-wrapper">
-              <h3>SMART CONTRACTS</h3>
+              <h3>BORED APE CLONE</h3>
               <p>
-                Add a little bit of body text. Add a little bit of body text.Add
-                a little bit of body text.
+                This is a clone of a very popualar NFT website. The project was
+                built using React and is being hosted on codesandbox.
               </p>
             </div>
           </div>
           <div className="projects_single-container">
-            <img className="projects_img" src={codeProject} />
+            <img className="projects_img" src={nft} />
             <div className="projects_description-wrapper">
-              <h3>SMART CONTRACTS</h3>
+              <h3>MINTING APP</h3>
               <p>
-                Add a little bit of body text. Add a little bit of body text.Add
-                a little bit of body text.
+                In this decentralized application, users are able to create,
+                purchase, and sell custom NFT's on a test blockchain network.
               </p>
             </div>
           </div>
@@ -35,10 +37,10 @@ const Projects = () => {
           <div className="projects_single-container">
             <img className="projects_img" src={codeProject} />
             <div className="projects_description-wrapper">
-              <h3>SMART CONTRACTS</h3>
-              <p className="projects_container-para">
-                Add a little bit of body text. Add a little bit of body text.Add
-                a little bit of body text.
+              <h3>PROPERTY NFT METAVERSE</h3>
+              <p>
+                In this decentralized application, users are able to create,
+                purchase, and sell custom NFT's on a test blockchain network.
               </p>
             </div>
           </div>
