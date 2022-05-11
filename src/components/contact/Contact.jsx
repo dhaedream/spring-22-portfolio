21c9faacaba492ad353218bc7bc770e509d9c9d2
Dhaesha M <dhaesha1@gmail.com>
2022-05-11 14:30:04 -0400

    contact styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,37 +6,45 @@ import targetIcon from "../../assets/contact-location.png";
 const Contact = () => {
   return (
     <div className="contact">
-      <div className="contact_inner-wrapper">
-        <div className="contact_cloud">
-          <img className="contact_cloud-img" src={cloud} />
-        </div>
+      {/* Will make seprate cloud component */}
+      {/* <div className="contact_cloud">
+        <img className="contact_cloud-img" src={cloud} />
+      </div> */}
+      <div className="contact_card">
         <div className="contact_open-span">
           <span className="contact_span-1">OPEN</span>
           <span className="contact_span-2">TO CONNECT</span>
         </div>
-        <div className="contact_card-outer-container">
-          <div className="contact_card">
-            <div className="contact_card-content-wrapper">
-              <div className="contact_card-main">
-                <div className="contact_card-row">
-                  <img className="" src={targetIcon} />
-                  <span>Washington D.C.</span>
-                </div>
-                <div className="contact_card-row">
-                  <img className="" src={targetIcon} />
-                  <span>Washington D.C.</span>
-                </div>
-                <div className="contact_card-row">
-                  <div className="contact_card-row">
-                    <img className="" src={targetIcon} />
-                    <span>Washington D.C.</span>
-                  </div>
-                </div>
-
-                <div className="contact_card-row">
-                  <img className="" src={targetIcon} />
-                  <span>Washington D.C.</span>
-                </div>
+        <div className="contact_card-content-wrapper">
+          <div className="contact_card-main">
+            <div className="contact_card-row-wrap">
+              <div className="contact_card-row">
+                <img className="" src={targetIcon} />
+                <span>Dhaesha Myers</span>
+              </div>
+            </div>
+            <div className="contact_card-row-wrap">
+              <div className="contact_card-row">
+                <img className="" src={targetIcon} />
+                <span>Washington D.C.</span>
+              </div>
+            </div>
+            <div className="contact_card-row-wrap">
+              <div className="contact_card-row">
+                <img className="" src={targetIcon} />
+                <span>202-550-1234</span>
+              </div>
+            </div>
+            <div className="contact_card-row-wrap">
+              <div className="contact_card-row">
+                <img className="" src={targetIcon} />
+                <span>My Github</span>
+              </div>
+            </div>
+            <div className="contact_card-row-wrap">
+              <div className="contact_card-row">
+                <img className="" src={targetIcon} />
+                <span>My LinkedIn</span>
               </div>
             </div>
           </div>
@@ -47,26 +55,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-{
-  /* <div className="contact_card-row">
-              <img src="" />
-              <h5>Dhae Myers</h5>
-            </div>
-            <div className="contact_card-row">
-              <img src="" />
-              <h5>Washington DC</h5>
-            </div>{" "}
-            <div className="contact_card-row">
-              <img src="" />
-              <h5>202-550-1234</h5>
-            </div>
-            <div className="contact_card-row">
-              <img src="" />
-              <h5>github</h5>
-            </div>{" "}
-            <div className="contact_card-row">
-              <img src="" />
-              <h5>linkedin</h5>
-            </div> */
-}
