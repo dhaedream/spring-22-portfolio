60d22f3998f81b9193ec962a1873119537b788ad
Dhaesha M <dhaesha1@gmail.com>
2022-05-15 15:40:05 -0400

    set header animation up in variant variable

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,19 +3,30 @@ import "./header.css";
 import bannerImg from "../../assets/banner-dark.png";
 import { motion } from "framer-motion";
 
+const headerMotion = {
+  screenstart: { y: -300 },
+  screeninit: { y: 0, transition: { duration: 1.5 } },
+};
+
 const Header = () => {
   return (
     <div className="header_container">
       <div className="header_top">
         <motion.h2
-          initial={{ y: -100 }}
-          animate={{ y: 0 }}
-          transition={{ duration: 2 }}
+          initial={"screenstart"}
+          animate={"screeninit"}
+          variants={headerMotion}
         >
           DHAE DREAM
         </motion.h2>
       </div>
-      <img src={bannerImg} className="header_banner-img" />
+      <motion.img
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 3 }}
+        src={bannerImg}
+        className="header_banner-img"
+      />
     </div>
   );
 };
