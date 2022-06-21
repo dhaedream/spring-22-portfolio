f2bc256cb65a089818a016cf579a927c58c8ffdf
Dhaesha M <dhaesha1@gmail.com>
2022-06-20 22:54:53 -0400

    changed some positions

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -9,29 +9,7 @@ const Features = () => {
   return (
     <div id="skills" className="features">
       <div className="features_single-column">
-        <img src={skillsIcon} className="features_icon-img" />
-        <div className="features_content-wrapper">
-          <h3 className="features_header">SKILLSET</h3>
-          <div className="features_body-para">
-            <p>Team Player</p>
-            <p>Developer/Designer</p>
-            <p>Blockchain Junky</p>
-          </div>
-        </div>
-      </div>
-      <div className="features_single-column">
-        <img src={fileIcon} className="features_icon-img" />
-        <div className="features_content-wrapper">
-          <h3 className="features_header">BACKGROUND</h3>
-          <p className="features_body-para">
-            <p>Lifelong Artist</p>
-            <p>5 years in IT Support</p>
-            <p>Freelance Graphic Design</p>
-          </p>
-        </div>
-      </div>
-      <div className="features_single-column">
-        <img src={techIcon} className="features_icon-img" />
+        <img src={techIcon} className="features_icon-img" alt="tech" />
         <div className="features_content-wrapper">
           <h3 className="features_header">TECHNOLOGIES</h3>
           <p className="features_body-para">
@@ -41,7 +19,28 @@ const Features = () => {
         </div>
       </div>
       <div className="features_single-column">
-        <img src={cloudIcon} className="features_icon-img" />
+        <img src={fileIcon} className="features_icon-img" alt="background" />
+        <div className="features_content-wrapper">
+          <h3 className="features_header">BACKGROUND</h3>
+          <p className="features_body-para">
+            <p>5 years of IT Support</p>
+            <p>Freelance Graphic Design</p>
+          </p>
+        </div>
+      </div>{" "}
+      <div className="features_single-column">
+        <img src={skillsIcon} className="features_icon-img" alt="skills" />
+        <div className="features_content-wrapper">
+          <h3 className="features_header">SKILLSET</h3>
+          <div className="features_body-para">
+            <p>Dynamic Self Starter</p>
+            <p>Developer/Designer</p>
+            <p>Blockchain Stack</p>
+          </div>
+        </div>
+      </div>
+      <div className="features_single-column">
+        <img src={cloudIcon} className="features_icon-img" alt="dreamer" />
         <div className="features_content-wrapper">
           <h3 className="features_header">MY DREAM</h3>
           <p className="features_body-para">
