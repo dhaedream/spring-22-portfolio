c5416266775d3fdeb026e2553fd6a2fd5edf34f9
Dhaesha M <dhaesha1@gmail.com>
2022-06-22 22:16:26 -0400

    more work on modal

--- src/components/proj-modal/ProjModal.jsx
+++ src/components/proj-modal/ProjModal.jsx
@@ -9,8 +9,38 @@ const ProjModal = ({ open, onClose, details }) => {
     <>
       <div className="modal_overlay">
         <div className="modal_container">
-          <img src={details.img} alt="" />
           <p onClick={onClose}>X</p>
+          <div className="modal_feature">
+            {/* <img src={details.img} alt="" /> */}
+            <span>React, HardHat, Ethers, Solidity, Typescript, Tailwind</span>
+          </div>
+
+          <div className="modal_content-wrap">
+            <h4>{details.title}</h4>
+
+            <div className="modal_details">
+              <p>
+                Jelly beans dragée cheesecake bear claw tart jelly beans gummies
+                I love icing. Cake marshmallow cheesecake wafer I love tootsie
+                roll danish I love cotton candy. I love carrot cake bonbon ice
+                cream dessert I love tart sesame snaps marzipan.
+              </p>
+              <p>
+                Cupcake ipsum dolor sit amet oat cake. Dragée shortbread oat
+                cake sweet gummies tootsie roll toffee. I love marshmallow donut
+                gummi bears chocolate cake. Ice cream cake candy canes topping
+                cookie bear claw.
+              </p>
+            </div>
+            <div className="modal_button-wrap">
+              <a href="./" target="_blank" rel="noreferrer">
+                <button className="modal_button">My custom button</button>
+              </a>
+              <a href="./" target="_blank" rel="noreferrer">
+                <button className="modal_button">My custom button</button>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </>
