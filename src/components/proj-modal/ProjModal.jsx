43e4201fef0666c1122729ec38f7918c4b8c151d
Dhaesha M <dhaesha1@gmail.com>
2022-06-22 21:18:44 -0400

    modal ops up in center of screen. overlay needs work

--- src/components/proj-modal/ProjModal.jsx
+++ src/components/proj-modal/ProjModal.jsx
@@ -3,15 +3,17 @@ import demo from "../../assets/greeter.png";
 import "./projmodal.css";
 
 const ProjModal = ({ open, onClose, details }) => {
-  console.log(`${demo}`);
+  console.log(`${details.title}`);
   if (!open) return null;
   return (
-    <div className="modal_overlay">
-      <div className="modal_container">
-        <img src={details.img} alt="" />
-        <p onClick={onClose}>X</p>
+    <>
+      <div className="modal_overlay">
+        <div className="modal_container">
+          <img src={details.img} alt="" />
+          <p onClick={onClose}>X</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
