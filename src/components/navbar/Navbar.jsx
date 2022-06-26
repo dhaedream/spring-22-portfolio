a90e629c3e9b2161ef9d3d431b7ccd0beabf5cff
Dhaesha M <dhaesha1@gmail.com>
2022-06-26 18:03:25 -0400

    will redo navbar later. revereted back to older version commit f43d12c3724765886fd6cfbbd360091be97cfe7a Showing 1 changed file with 14 additions and 9 deletions.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "./navbar.css";
 import ddLogo from "../../assets/dd-logo.png";
 import disLogo from "../../assets/discord-logo.png";
 import gitLogo from "../../assets/github-logo.png";
 import linLogo from "../../assets/linkedin-logo.png";
-import { Icon } from "react-icons-kit";
-// horiz icon
-import { more } from "react-icons-kit/ikons/more";
-// vert icon
-import { more_2 } from "react-icons-kit/ikons/more_2";
+
+// LogoLinkedin, LogoDiscord
 
 const Navbar = () => {
-  const [popMenu, setPopMenu] = useState(false);
-  const handleMenu = () => {
-    setPopMenu(!popMenu);
-    console.log("working");
-  };
-
   return (
     <div className="navbar_container">
-      <img src={ddLogo} href="home" id="ddLogo" alt="" />
-
-      <div
-        className={popMenu ? "navbar_links-closed" : "navbar_links-container"}
-      >
+      <img src={ddLogo} href="home" id="ddLogo" />
+      <div className="navbar_links-container">
         <p className="navbar_link">
           <a href="#about">ABOUT</a>
         </p>
@@ -37,33 +25,24 @@ const Navbar = () => {
           <a href="#contact">CONTACT</a>
         </p>
       </div>
-
       <div className="navbar_logo-container">
         <a
           href="https://linkedin.com/in/dhaesha"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={linLogo} alt="" />
+          <img src={linLogo} />
         </a>
         <a
           href="https://discord.com/users/961022680776196106"
           target="_blank"
           rel="noreferrer"
         >
-          <img src={disLogo} alt="" />
+          <img src={disLogo} />
         </a>
         <a href="https://github.com/dhaedream" target="_blank" rel="noreferrer">
-          <img src={gitLogo} alt="" />
+          <img src={gitLogo} />
         </a>
-      </div>
-
-      <div className="navbar-menu-container" onClick={handleMenu}>
-        {popMenu ? (
-          <Icon icon={more} size={28} />
-        ) : (
-          <Icon icon={more_2} size={28} />
-        )}
       </div>
     </div>
   );
