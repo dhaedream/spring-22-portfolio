0f0e36bdc73edccc9a965ac606e110a04e292f14
Dhaesha M <dhaesha1@gmail.com>
2022-07-03 16:30:56 -0400

    the animation broke my buttons until I wrapped it in a div. brought the buttons back to life and fit it better on smaller screens.

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -17,39 +17,41 @@ const Project = (props) => {
         className="projects_img"
         src={details.img}
         alt="boredApe"
-        onClick={() => setOpenModal(true)}
+        // onClick={() => setOpenModal(true)}
       />
       <div className="projects_description-wrapper">
         <h3>{details.title}</h3>
         {/* <p>{details.description}</p> */}
-        <motion.button
-          animate={{
-            boxShadow: [
-              "0 0 4px #fff",
-              "0 0 11px #fff",
-              "0 0 19px #fff",
-              "0 0 40px #0fa",
-              "0 0 80px #0fa",
-              "0 0 90px #0fa",
-              "0 0 100px #0fa",
-              "0 0 150px #0fa",
-            ],
-            opacity: [0.2, 0.5, 1],
-          }}
-          transition={{ duration: 1, repeat: Infinity, repeatDelay: 0 }}
-          //@dev moving component
-          // style when page first loads
-          initial={"screenstart"}
-          // style while in view
-          whileInView={"screeninit"}
-          // viewport tru plays multiple times. false play oonce
-          viewport={{ once: false, amount: 0.1 }}
-          variants={headerMotion}
-          onClick={() => setOpenModal(true)}
-          className="projects_button"
-        >
-          More
-        </motion.button>
+        <div>
+          <motion.button
+            onClick={() => setOpenModal(true)}
+            animate={{
+              boxShadow: [
+                "0 0 4px #fff",
+                "0 0 11px #fff",
+                "0 0 19px #fff",
+                "0 0 40px #0fa",
+                "0 0 80px #0fa",
+                "0 0 90px #0fa",
+                "0 0 100px #0fa",
+                "0 0 150px #0fa",
+              ],
+              opacity: [0.2, 0.5, 1],
+            }}
+            transition={{ duration: 1, repeat: Infinity, repeatDelay: 0 }}
+            //@dev moving component
+            // style when page first loads
+            initial={"screenstart"}
+            // style while in view
+            whileInView={"screeninit"}
+            // viewport tru plays multiple times. false play oonce
+            viewport={{ once: false, amount: 0.1 }}
+            variants={headerMotion}
+            className="projects_button"
+          >
+            More
+          </motion.button>
+        </div>
       </div>
       <ProjModal
         details={details}
